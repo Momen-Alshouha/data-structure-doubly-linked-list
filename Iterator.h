d33a824a5f31d9b97d59a4faa5b7791d7baaaf6c
momen-alshouha <momenalshouha@outlook.com>
2024-01-10 20:24:19 +0300

    iterator class prepared

---
+++ Iterator.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Node.h"
+
+template<typename T>
+class Iterator {
+	
+	Node<T>* _CurrentNode;
+
+public:
+	
+	Iterator() :_CurrentNode(nullptr) {};
+	
+	Iterator(Node<T>* CurrentNode) :_CurrentNode(CurrentNode){};
+
+	Node<T>* GetCurrenctNode() {
+		return _CurrentNode;
+	}
+
+	T GetData()
+	{
+		return _CurrentNode->data;
+	}
+
+	void SetData(T Data) {
+		_CurrentNode->data = Data;
+	}
+
+	Node<T>* Next() {
+		if (_CurrentNode != nullptr)
+		{
+			_CurrentNode = _CurrentNode->next;
+			return _CurrentNode;
+		}
+		return nullptr;
+	}
+
+	__declspec(property(get = GetData, put = SetData)) T data;
+	__declspec(property(get = GetCurrentNode)) Node<T>* current_node;
+
+};
