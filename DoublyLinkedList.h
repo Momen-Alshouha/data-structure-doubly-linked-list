7fd7718717e601f99d0f0543ee213f81d20b372a
momen-alshouha <momenalshouha@outlook.com>
2024-01-11 09:03:56 +0300

    implement find , IsExisit and insetion method in DoublyLinkedList class

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <iostream>
 #include "Node.h"
 #include "Iterator.h"
+using namespace std;
 
 template<typename T>
 class DoublyLinkedList
@@ -9,7 +11,7 @@ class DoublyLinkedList
 
 	Node<T>* _Tail = nullptr;
 
-	short _Length=0;
+	short _Length = 0;
 
 	Iterator<T> _begin() {
 		return Iterator<T>(_Head);
@@ -46,6 +48,121 @@ public:
 	__declspec(property(get = GetHead)) Node<T>* head;
 	__declspec(property(get = GetTail)) Node<T>* tail;
 
+	Node<T>* Find(T DataToFind) {
+		for (Iterator<T> itr = _begin(); itr != _end(); itr.Next())
+		{
+			if (itr.data == DataToFind)
+			{
+				return itr.current_node;
+			}
+		}
+		return nullptr;
+	}
+
+
+	bool isExisit(T DataToFind) {
+		Node<T>* NodeToFind = Find(DataToFind);
+
+		if (NodeToFind != nullptr)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	void InsertBegin(T DataToInsert) {
+
+		Node<T>* NewNode = new(nothrow) Node<T>(DataToInsert);
+		if (NewNode==nullptr)
+		{
+			cout << "Error Memory Allocation\n";
+			return;
+		}
+
+		if (_Head==nullptr)
+		{
+			_Head = _Tail = NewNode;
+			return;
+		}
+
+		NewNode->next = _Head;
+		_Head->prev = NewNode;
+		_Head = NewNode;
+	
+	};
+
+	void InsertEnd(T DataToInsert) {
+		Node<T>* NewNode = new(nothrow) Node<T>(DataToInsert);
+
+		if (NewNode == nullptr)
+		{
+			cout << "Error Memory Allocation\n";
+			return;
+		}
+
+		if (_Head==nullptr)
+		{
+			_Head = _Tail = NewNode;
+			return;
+		}
+
+		_Tail->next = NewNode;
+		NewNode->prev = _Tail;
+		_Tail = NewNode;
+	}
+
+	void InsertBefore(T DataToInsertBefore,T NewData) {
+		
+		Node<T>* NodeToInsertBefore = Find(DataToInsertBefore);
+		
+		if (NodeToInsertBefore==nullptr) // if node not found
+		{
+			return;
+		}
+
+		Node<T>* NewNode = new Node<T>(NewData);
+
+		if (NodeToInsertBefore==_Head)
+		{
+			NewNode->next = _Head;
+			_Head->prev = NewNode;
+			_Head = NewNode;
+			return;
+
+		}
+	
+		NewNode->next = NodeToInsertBefore;
+		NewNode->prev = NodeToInsertBefore->prev;
+
+		if (NodeToInsertBefore->prev != nullptr) {
+			NodeToInsertBefore->prev->next = NewNode;
+		}
+
+		NodeToInsertBefore->prev = NewNode;
+
+	}
+
+	void InsertAfter(T DataToInsertAfter) {
+
+
+	}
+
+	void InsertAtIndex(short index) {
+
+	}
+
+	
+	void Print() {
+		for (Iterator<T> itr = _begin(); itr != _end(); itr.Next())
+		{
+			cout << itr.data;
+			if (itr.current_node!=_Tail)
+			{
+				cout << " -> ";
+			}
+		}
+		cout << endl;
+	}
 };
 
 
--- data-structures-doubly-linked-list.cpp
+++ data-structures-doubly-linked-list.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+
 #include "DoublyLinkedList.h"
-using namespace std;
+
 
 int main()
 {
-	DoublyLinkedList<int> doublyLinkedList;
-	cout << doublyLinkedList.length << endl;
+	DoublyLinkedList<int> dLinkedList;
+	
+	dLinkedList.InsertEnd(1);
+	dLinkedList.InsertEnd(2);
+	dLinkedList.InsertEnd(3);
+	dLinkedList.InsertEnd(4);
+	dLinkedList.InsertBefore(2, 99);
+	cout << boolalpha << dLinkedList.isExisit(99) << endl;
+	cout << boolalpha << dLinkedList.isExisit(999) << endl;
+	dLinkedList.Print();
+	cout << dLinkedList.length << endl;
+	cout << dLinkedList.head->data << endl;
+	cout << dLinkedList.tail->data << endl;
 }
